721fe43d298336c28101e1cc040912b6d5bf5ec3
Ruben <ruben.escudero@gmail.com>
2014-10-17 10:10:14 -0700

    cleanup

--- Readme.md
+++ Readme.md
@@ -12,25 +12,25 @@
 		
 		APIs (curl):
 			//Create a todo
-			curl -i -X POST -H 'Content-Type: application/json' -d '{"title":"Todo Title", "description":"Todo Desc", "done": false}' http://localhost:8080/todo-app/todo
+			curl -i -X POST -H 'Content-Type: application/json' -d '{"title":"Todo Title", "description":"Todo Desc", "done": false}' http://localhost:8080/todo-api/v1/todo
 
 			//Retrive all todo's
-			curl -i -X GET -H 'Content-Type: application/json' http://localhost:8080/todo-app/todo
+			curl -i -X GET -H 'Content-Type: application/json' http://localhost:8080/todo-api/v1/todo
 
 			//find todo by id
-			curl -i -X GET -H 'Content-Type: application/json' http://localhost:8080/todo-app/todo/543ec1eb0364f8ca5dd372af
+			curl -i -X GET -H 'Content-Type: application/json' http://localhost:8080/todo-api/v1/todo/543ec1eb0364f8ca5dd372af
 
 			//update todo
-			curl -i -X PUT -H 'Content-Type: application/json' -d '{"title":"Todo Title update", "description":"Todo Desc update", "done": true}' http://localhost:8080/todo-app/todo/543ec1eb0364f8ca5dd372af
+			curl -i -X PUT -H 'Content-Type: application/json' -d '{"title":"Todo Title update", "description":"Todo Desc update", "done": true}' http://localhost:8080/todo-api/v1/todo/543ec1eb0364f8ca5dd372af
 
 			//find updated todo by id
-			curl -i -X GET -H 'Content-Type: application/json' http://localhost:8080/todo-app/todo/543ec1eb0364f8ca5dd372af
+			curl -i -X GET -H 'Content-Type: application/json' http://localhost:8080/todo-api/v1/todo/543ec1eb0364f8ca5dd372af
 
 			//delete todo
-			curl -i -X DELETE -H 'Content-Type: application/json' http://localhost:8080/todo-app/todo/543ec1eb0364f8ca5dd372af
+			curl -i -X DELETE -H 'Content-Type: application/json' http://localhost:8080/todo-api/v1/todo/543ec1eb0364f8ca5dd372af
 
 			//verify todo doesn't exist
-			curl -i -X GET -H 'Content-Type: application/json' http://localhost:8080/todo-app/todo/543ec1eb0364f8ca5dd372af
+			curl -i -X GET -H 'Content-Type: application/json' http://localhost:8080/todo-api/v1/todo/543ec1eb0364f8ca5dd372af
 
 
 	Database: MongoDB
--- src/main/java/com/todo/api/exceptions/ErrorMessage.java
+++ src/main/java/com/todo/api/exceptions/ErrorMessage.java
@@ -1,6 +1,8 @@
 package com.todo.api.exceptions;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
@@ -9,87 +11,89 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import org.apache.commons.beanutils.BeanUtils;
 
-
 @XmlRootElement
 public class ErrorMessage {
-	
-	/** contains the same HTTP Status code returned by the server */
-	@XmlElement(name = "status")
-	int status;
-	
-	/** application specific error code */
-	@XmlElement(name = "code")
-	int code;
-	
-	/** message describing the error*/
-	@XmlElement(name = "message")
-	String message;
-		
-	/** link point to page where the error message is documented */
-	@XmlElement(name = "link")
-	String link;
-	
-	/** extra information that might useful for developers */
-	@XmlElement(name = "developerMessage")
-	String developerMessage;	
 
-	public int getStatus() {
-		return status;
-	}
+    /**
+     * contains the same HTTP Status code returned by the server
+     */
+    @XmlElement(name = "status")
+    int status;
+    /**
+     * application specific error code
+     */
+    @XmlElement(name = "code")
+    int code;
+    /**
+     * message describing the error
+     */
+    @XmlElement(name = "message")
+    String message;
+    /**
+     * link point to page where the error message is documented
+     */
+    @XmlElement(name = "link")
+    String link;
+    /**
+     * extra information that might useful for developers
+     */
+    @XmlElement(name = "developerMessage")
+    String developerMessage;
 
-	public void setStatus(int status) {
-		this.status = status;
-	}
+    public int getStatus() {
+        return status;
+    }
 
-	public int getCode() {
-		return code;
-	}
+    public void setStatus(int status) {
+        this.status = status;
+    }
 
-	public void setCode(int code) {
-		this.code = code;
-	}
+    public int getCode() {
+        return code;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public void setCode(int code) {
+        this.code = code;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public String getDeveloperMessage() {
-		return developerMessage;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-	public void setDeveloperMessage(String developerMessage) {
-		this.developerMessage = developerMessage;
-	}
+    public String getDeveloperMessage() {
+        return developerMessage;
+    }
 
-	public String getLink() {
-		return link;
-	}
+    public void setDeveloperMessage(String developerMessage) {
+        this.developerMessage = developerMessage;
+    }
 
-	public void setLink(String link) {
-		this.link = link;
-	}
-	
-	public ErrorMessage(AppException ex){
-		try {
-			BeanUtils.copyProperties(this, ex);
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public ErrorMessage(NotFoundException ex){
-		this.status = Response.Status.NOT_FOUND.getStatusCode();
-		this.message = ex.getMessage();
-		this.link = "https://jersey.java.net/apidocs/2.8/jersey/javax/ws/rs/NotFoundException.html";		
-	}
+    public String getLink() {
+        return link;
+    }
 
-	public ErrorMessage() {}
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public ErrorMessage(AppException ex) {
+        try {
+            BeanUtils.copyProperties(this, ex);
+        } catch (Exception ex1) {
+            Logger.getLogger(ErrorMessage.class.getName()).log(Level.SEVERE, null, ex1);
+        }
+    }
+
+    public ErrorMessage(NotFoundException ex) {
+        this.status = Response.Status.NOT_FOUND.getStatusCode();
+        this.message = ex.getMessage();
+        this.link = "https://jersey.java.net/apidocs/2.8/jersey/javax/ws/rs/NotFoundException.html";
+    }
+
+    public ErrorMessage() {
+    }
 }
